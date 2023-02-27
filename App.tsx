a17260e64af56bec7c8699786f1b1bf7ef0129bc
omkar342 <omkarjadhav095@gmail.com>
2023-02-28 01:31:43 +0530

    Created ElevatedCards.tsx and imported it in App.tsx

--- App.tsx
+++ App.tsx
@@ -11,6 +11,7 @@ import React from 'react';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 import FlatCards from './Components/FlatCards';
+import ElevatedCards from './Components/ElevatedCards';
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -20,6 +21,7 @@ function App(): JSX.Element {
       <ScrollView>
         <View>
           <FlatCards />
+          <ElevatedCards />
         </View>
       </ScrollView>
     </SafeAreaView>
---
+++ Components/ElevatedCards.tsx
@@ -0,0 +1,66 @@
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const ElevatedCards = () => {
+  return (
+    <View>
+      <Text style={styles.headerText}>ElevatedCards</Text>
+      <ScrollView style={styles.container} horizontal>
+        <View style={[styles.card, styles.elevatedCards]}>
+          <Text>Tap</Text>
+        </View>
+        <View style={[styles.card, styles.elevatedCards]}>
+          <Text>me</Text>
+        </View>
+        <View style={[styles.card, styles.elevatedCards]}>
+          <Text>to</Text>
+        </View>
+        <View style={[styles.card, styles.elevatedCards]}>
+          <Text>scroll</Text>
+        </View>
+        <View style={[styles.card, styles.elevatedCards]}>
+          <Text>more</Text>
+        </View>
+        <View style={[styles.card, styles.elevatedCards]}>
+          <Text>...</Text>
+        </View>
+        <View style={[styles.card, styles.elevatedCards]}>
+          <Text>!</Text>
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+export default ElevatedCards;
+
+const styles = StyleSheet.create({
+  headerText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  container: {
+    padding: 5,
+  },
+  card: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 100,
+    width: 100,
+    margin: 10,
+    borderRadius: 10,
+  },
+  elevatedCards: {
+    backgroundColor: '#25D366',
+    elevation: 10,
+    shadowOffset: {
+      width: 20,
+      height: 2,
+    },
+    shadowColor: '#ff000',
+    shadowRadius: 5,
+    shadowOpacity: 0.5,
+  },
+});
